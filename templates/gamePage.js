37d731732bac05a6403215d9be1fc015d4c819cf
SoilChang <im741314@gmail.com>
2015-05-26 17:28:40 +0800

    26/05 5:28pm

--- templates/gamePage.js
+++ templates/gamePage.js
@@ -1,36 +1,46 @@
 $(document).ready(function(){
 	// hide all the center preview
-	$('#c-kingdom').hide();
+	$('#c-kingdom,#c-Rules,#c-Tower_Type,#c-Allies,#c-shop').hide();
+
 
 	//click at b1 tab, kingdom preview should show
 	$('#b1').click(function(){
-		$('#c-kingdom').show(function(){
+		$('#c-kingdom').show();
+		$('#c-Rules').hide();
+		$('#c-Tower_Type').hide();
+		$('#c-Allies').hide();
+		$('#c-shop').hide();
+	});
 
-			// the following code is to add input text status to the display message
-			$('#c-kingdom-status-button').click(function(){
-				var inputText = $('input[name=inputText]').val();
-				$('#c-kingdom-status-display').html(inputText);
-			});
+	$('#b2').click(function(){
+		$('#c-kingdom').hide();
+		$('#c-Rules').show();
+		$('#c-Tower_Type').hide();
+		$('#c-Allies').hide();
+		$('#c-shop').hide();
+	});
 
-			// the following is the functionality specific to close button
-			$('#c-kingdom-closeButton').hover(function(){
-				// when hover
-				$('#c-kingdom-closeButton').animate(
-					{	'height':'40px','left':'95%','top':'0%',},'fast');
-				$('#c-kingdom-closeButton').click(function(){
-					$('#c-kingdom').hide();
-				});
-			},
-				// when unhover
-				function(){
-					$('#c-kingdom-closeButton').animate(
-					{	'height':'30px','left':'96%','top':'0%',},'fast');
-				}
-			);
-			
-		});
+	$('#b3').click(function(){
+		$('#c-kingdom').hide();
+		$('#c-Rules').hide();
+		$('#c-Tower_Type').show();
+		$('#c-Allies').hide();
+		$('#c-shop').hide();
+	});
 
+	$('#b4').click(function(){
+		$('#c-kingdom').hide();
+		$('#c-Rules').hide();
+		$('#c-Tower_Type').hide();
+		$('#c-Allies').show();
+		$('#c-shop').hide();
+	});
 
-
+	$('#b5').click(function(){
+		$('#c-kingdom').hide();
+		$('#c-Rules').hide();
+		$('#c-Tower_Type').hide();
+		$('#c-Allies').hide();
+		$('#c-shop').show();
 	});
 });
