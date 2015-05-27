98075fc3f8b7e3f399ea0faa3e64866dfccc2dfc
SoilChang <im741314@gmail.com>
2015-05-28 07:54:03 +0800

    28/05 753am

--- templates/gamePage.js
+++ templates/gamePage.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	// hide all the center preview
-	$('#c-kingdom,#c-Rules,#c-Tower_Type,#c-Allies,#c-shop').hide();
+	$('#c-kingdom,#c-Rules,#c-Tower_Type,#c-Allies,#c-shop,#c-game').hide();
 
 
 	//click at b1 tab, kingdom preview should show
@@ -10,6 +10,7 @@ $(document).ready(function(){
 		$('#c-Tower_Type').hide();
 		$('#c-Allies').hide();
 		$('#c-shop').hide();
+		$('#c-game').hide();
 	});
 
 	$('#b2').click(function(){
@@ -18,6 +19,7 @@ $(document).ready(function(){
 		$('#c-Tower_Type').hide();
 		$('#c-Allies').hide();
 		$('#c-shop').hide();
+		$('#c-game').hide();
 	});
 
 	$('#b3').click(function(){
@@ -26,6 +28,7 @@ $(document).ready(function(){
 		$('#c-Tower_Type').show();
 		$('#c-Allies').hide();
 		$('#c-shop').hide();
+		$('#c-game').hide();
 	});
 
 	$('#b4').click(function(){
@@ -34,6 +37,7 @@ $(document).ready(function(){
 		$('#c-Tower_Type').hide();
 		$('#c-Allies').show();
 		$('#c-shop').hide();
+		$('#c-game').hide();
 	});
 
 	$('#b5').click(function(){
@@ -42,5 +46,15 @@ $(document).ready(function(){
 		$('#c-Tower_Type').hide();
 		$('#c-Allies').hide();
 		$('#c-shop').show();
+		$('#c-game').hide();
+	});
+
+	$('#b6').click(function(){
+		$('#c-kingdom').hide();
+		$('#c-Rules').hide();
+		$('#c-Tower_Type').hide();
+		$('#c-Allies').hide();
+		$('#c-shop').hide();
+		$('#c-game').show();
 	});
 });
