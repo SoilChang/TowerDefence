9db640bb82e9e8f4e9464869b00e0ff379aa9247
Dylan Chew <dylanchewzj@hotmail.com>
2015-06-01 02:41:18 +0800

    1 jun 2.41am
    -added grid to whole map

--- GamePrototype/game.js
+++ GamePrototype/game.js
@@ -1,9 +1,224 @@
 
-var stage, mapData, hitsT, hit1, hit2, hit3, hit4, hit5,
+var stage, mapData, hitsT, hit0, hit1, hit2, hit3, hit4, hit5, hit6, hit7, hit8, hit9
 tileset, output, cash, life, coordinates, 
 castleI, castle,
 heroI, hero, tower, towerCost, towerI, towerSelection,
-monsterI, monsters, monstersAmt, monster1, monster2
+monsterI, monsters, monstersAmt
+
+//grid
+function grid() {
+    //hitarea of map
+    hitsT=[]
+    //left area
+    hitsT[0] = []
+    hit0=[2, stage.canvas.height/32]
+
+    for (var i=0;i<hit0[0];i++) {
+        hitsT[0][i] = [];
+    };
+
+    for (var i=0;i<hit0[0];i++) {
+        for (var j=0;j<hit0[1];j++) {
+            hitsT[0][i][j] = new createjs.Shape();
+            hitsT[0][i][j].graphics.beginFill("#f00").drawRect(32*i,32*j,32,32);
+            hitsT[0][i][j].alpha=0.01;
+            hitsT[0][i][j].coord=[32*i,32*j]
+            hitsT[0][i][j].on("mouseover", handleMouse);
+            hitsT[0][i][j].on("mouseout", handleMouse);
+            hitsT[0][i][j].on("click", handleMouse); 
+            stage.addChild(hitsT[0][i][j]);
+        };
+    };
+    //bottom area
+    hitsT[1] = []
+    hit1=[(stage.canvas.width/32)-4,1]
+
+    for (var i=0;i<hit1[0];i++) {
+        hitsT[1][i] = [];
+    };
+
+    for (var i=0;i<hit1[0];i++) {
+        for (var j=0;j<hit1[1];j++) {
+            hitsT[1][i][j] = new createjs.Shape();
+            hitsT[1][i][j].graphics.beginFill("#f00").drawRect(64+32*i,
+                (stage.canvas.height-32),32,32);
+            hitsT[1][i][j].alpha=0.01;
+            hitsT[1][i][j].coord=[64+32*i,stage.canvas.height-32];
+            hitsT[1][i][j].on("mouseover", handleMouse);
+            hitsT[1][i][j].on("mouseout", handleMouse);
+            hitsT[1][i][j].on("click", handleMouse); 
+            stage.addChild(hitsT[1][i][j]);
+        };
+    };
+    //top area
+    hitsT[2] = []
+    hit2=[(stage.canvas.width/32)-6,2]
+
+    for (var i=0;i<hit2[0];i++) {
+        hitsT[2][i] = [];
+    };
+
+    for (var i=0;i<hit2[0];i++) {
+        for (var j=0;j<hit2[1];j++) {
+            hitsT[2][i][j] = new createjs.Shape();
+            hitsT[2][i][j].graphics.beginFill("#f00").drawRect(128+32*i,
+                32*j,32,32);
+            hitsT[2][i][j].alpha=0.01;
+            hitsT[2][i][j].coord=[128+32*i,32*j];
+            hitsT[2][i][j].on("mouseover", handleMouse);
+            hitsT[2][i][j].on("mouseout", handleMouse);
+            hitsT[2][i][j].on("click", handleMouse); 
+            stage.addChild(hitsT[2][i][j]);
+        };
+    };
+    //left inner area
+    hitsT[3] = []
+    hit3=[2,12]
+
+    for (var i=0;i<hit3[0];i++) {
+        hitsT[3][i] = [];
+    };
+
+    for (var i=0;i<hit3[0];i++) {
+        for (var j=0;j<hit3[1];j++) {
+            hitsT[3][i][j] = new createjs.Shape();
+            hitsT[3][i][j].graphics.beginFill("#f00").drawRect(128+32*i,
+                64+32*j,32,32);
+            hitsT[3][i][j].alpha=0.01;
+            hitsT[3][i][j].coord=[128+32*i,64+32*j];
+            hitsT[3][i][j].on("mouseover", handleMouse);
+            hitsT[3][i][j].on("mouseout", handleMouse);
+            hitsT[3][i][j].on("click", handleMouse); 
+            stage.addChild(hitsT[3][i][j]);
+        };
+    };       
+    //bottom inner area
+    hitsT[4] = []
+    hit4=[16,2]
+
+    for (var i=0;i<hit4[0];i++) {
+        hitsT[4][i] = [];
+    };
+
+    for (var i=0;i<hit4[0];i++) {
+        for (var j=0;j<hit4[1];j++) {
+            hitsT[4][i][j] = new createjs.Shape();
+            hitsT[4][i][j].graphics.beginFill("#f00").drawRect(192+32*i,
+                stage.canvas.height-160+32*j,32,32);
+            hitsT[4][i][j].alpha=0.01;
+            hitsT[4][i][j].coord=[192+32*i,stage.canvas.height-160+32*j];
+            hitsT[4][i][j].on("mouseover", handleMouse);
+            hitsT[4][i][j].on("mouseout", handleMouse);
+            hitsT[4][i][j].on("click", handleMouse); 
+            stage.addChild(hitsT[4][i][j]);
+        };
+    };
+    //top inner area
+    hitsT[5] = []
+    hit5=[14,2]
+
+    for (var i=0;i<hit5[0];i++) {
+        hitsT[5][i] = [];
+    };
+
+    for (var i=0;i<hit5[0];i++) {
+        for (var j=0;j<hit5[1];j++) {
+            hitsT[5][i][j] = new createjs.Shape();
+            hitsT[5][i][j].graphics.beginFill("#f00").drawRect(256+32*i,
+                128+32*j,32,32);
+            hitsT[5][i][j].alpha=0.01;
+            hitsT[5][i][j].coord=[256+32*i,128+32*j];
+            hitsT[5][i][j].on("mouseover", handleMouse);
+            hitsT[5][i][j].on("mouseout", handleMouse);
+            hitsT[5][i][j].on("click", handleMouse); 
+            stage.addChild(hitsT[5][i][j]);
+        };
+    };
+    //bot inner-most area
+    hitsT[6] = []
+    hit6=[12,2]
+
+    for (var i=0;i<hit6[0];i++) {
+        hitsT[6][i] = [];
+    };
+
+    for (var i=0;i<hit6[0];i++) {
+        for (var j=0;j<hit6[1];j++) {
+            hitsT[6][i][j] = new createjs.Shape();
+            hitsT[6][i][j].graphics.beginFill("#f00").drawRect(256+32*i,
+                256+32*j,32,32);
+            hitsT[6][i][j].alpha=0.01;
+            hitsT[6][i][j].coord=[256+32*i,256+32*j];
+            hitsT[6][i][j].on("mouseover", handleMouse);
+            hitsT[6][i][j].on("mouseout", handleMouse);
+            hitsT[6][i][j].on("click", handleMouse); 
+            stage.addChild(hitsT[6][i][j]);
+        };
+    };
+    //left inner-most area
+    hitsT[7] = []
+    hit7=[2,2]
+
+    for (var i=0;i<hit7[0];i++) {
+        hitsT[7][i] = [];
+    };
+
+    for (var i=0;i<hit7[0];i++) {
+        for (var j=0;j<hit7[1];j++) {
+            hitsT[7][i][j] = new createjs.Shape();
+            hitsT[7][i][j].graphics.beginFill("#f00").drawRect(256+32*i,
+                192+32*j,32,32);
+            hitsT[7][i][j].alpha=0.01;
+            hitsT[7][i][j].coord=[256+32*i,192+32*j];
+            hitsT[7][i][j].on("mouseover", handleMouse);
+            hitsT[7][i][j].on("mouseout", handleMouse);
+            hitsT[7][i][j].on("click", handleMouse); 
+            stage.addChild(hitsT[7][i][j]);
+        };
+    };
+    //right inner area
+    hitsT[8] = []
+    hit8=[2,10]
+
+    for (var i=0;i<hit8[0];i++) {
+        hitsT[8][i] = [];
+    };
+
+    for (var i=0;i<hit8[0];i++) {
+        for (var j=0;j<hit8[1];j++) {
+            hitsT[8][i][j] = new createjs.Shape();
+            hitsT[8][i][j].graphics.beginFill("#f00").drawRect(704+32*i,
+                128+32*j,32,32);
+            hitsT[8][i][j].alpha=0.01;
+            hitsT[8][i][j].coord=[704+32*i,128+32*j];
+            hitsT[8][i][j].on("mouseover", handleMouse);
+            hitsT[8][i][j].on("mouseout", handleMouse);
+            hitsT[8][i][j].on("click", handleMouse); 
+            stage.addChild(hitsT[8][i][j]);
+        };
+    };
+    //right area
+    hitsT[9] = []
+    hit9=[2,17]
+
+    for (var i=0;i<hit9[0];i++) {
+        hitsT[9][i] = [];
+    };
+
+    for (var i=0;i<hit9[0];i++) {
+        for (var j=0;j<hit9[1];j++) {
+            hitsT[9][i][j] = new createjs.Shape();
+            hitsT[9][i][j].graphics.beginFill("#f00").drawRect(832+32*i,
+                32*j,32,32);
+            hitsT[9][i][j].alpha=0.01;
+            hitsT[9][i][j].coord=[704+32*i,128+32*j];
+            hitsT[9][i][j].on("mouseover", handleMouse);
+            hitsT[9][i][j].on("mouseout", handleMouse);
+            hitsT[9][i][j].on("click", handleMouse); 
+            stage.addChild(hitsT[9][i][j]);
+        };
+    };
+};
 
 //initialized
 function init() {
@@ -12,48 +227,7 @@ function init() {
 	//background image
 	mapData = level1;
 
-    //hitarea of map
-    hitsT=[]
-    hitsT[0] = []
-    hit1=[2, stage.canvas.height/32]
-
-    for (var i=0;i<hit1[0];i++) {
-        hitsT[0][i] = [];
-    };
-
-    for (var i=0;i<hit1[0];i++) {
-        for (var j=0;j<hit1[1];j++) {
-            hitsT[0][i][j] = new createjs.Shape();
-            hitsT[0][i][j].graphics.beginFill("#f00").drawRect(32*i,32*j,32,32);
-            hitsT[0][i][j].alpha=0.1;
-            hitsT[0][i][j].coord=[32*i,32*j]
-            hitsT[0][i][j].on("mouseover", handleMouse);
-            hitsT[0][i][j].on("mouseout", handleMouse);
-            hitsT[0][i][j].on("click", handleMouse); 
-            stage.addChild(hitsT[0][i][j]);
-        };
-    };
-
-    /*hitsT[1] = []
-    hit2=[(stage.canvas.width/32)-2,1]
-
-    for (var i=0;i<hit2[0];i++) {
-        hitsT[1][i] = [];
-    };
-
-    for (var i=0;i<hit2[0];i++) {
-        for (var j=0;j<hit2[1];j++) {
-            hitsT[1][i][j] = new createjs.Shape();
-            hitsT[1][i][j].graphics.beginFill("#f00").drawRect(64+32*i,
-                (stage.canvas.height-32),32,32);
-            hitsT[1][i][j].alpha=0.1;
-            hitsT[1][i][j].on("mouseover", handleMouse);
-            hitsT[1][i][j].on("mouseout", handleMouse);
-            stage.addChild(hitsT[1][i][j]);
-        };
-    };*/
-
-
+    grid();
 	//coordinates of creep movement
 	coordinates = [
 	[96, 0],
@@ -133,7 +307,7 @@ function tower(index) {
 
 //hit area
 function handleMouse(event) {
-    event.target.alpha = (event.type == "mouseover") ? .4 : 0.01;
+    event.target.alpha = (event.type == "mouseover") ? .3 : 0.01;
     if (event.type == "click") {
         if (towerSelection) {
             tower = new createjs.Bitmap(towerSelection[0]);
